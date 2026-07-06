711d299207b1eb835c42f437eded8604662034e6
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-07-06 11:46:23 +0900

    fix

--- packages/emoji-data/scripts/emoji-regex-generator/data.ts
+++ packages/emoji-data/scripts/emoji-regex-generator/data.ts
@@ -9,7 +9,7 @@ export const forceTreatAsTextDefault = [
     '2122',
 ];
 
-/** 後方互換性のための特別対応用（同一として扱う） */
+/** 後方互換性のための特別対応用（エイリアスとして扱う） */
 export const additionalEmojiSequencesByUnicode: Record<string, string[]> = {
     // https://github.com/jdecked/twemoji/issues/151, https://github.com/jdecked/twemoji-parser/pull/10
     '1f441-fe0f-200d-1f5e8-fe0f': [
